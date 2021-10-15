e9dc6cd5618fb40f813f17a68336622a4acdb7bb
EC2 Default User <ec2-user@ip-172-31-46-244.us-east-2.compute.internal>
2021-10-15 03:25:44 +0000

    part 1 completed

--- rottenpotatoes/app/controllers/movies_controller.rb
+++ rottenpotatoes/app/controllers/movies_controller.rb
@@ -1,47 +1,86 @@
 class MoviesController < ApplicationController
 
-  def show
-    id = params[:id] # retrieve movie ID from URI route
-    @movie = Movie.find(id) # look up movie by unique ID
-    # will render app/views/movies/show.<extension> by default
-  end
-
-  def index
-    @movies = Movie.all
-  end
-
-  def new
-    # default: render 'new' template
-  end
-
-  def create
-    @movie = Movie.create!(movie_params)
-    flash[:notice] = "#{@movie.title} was successfully created."
-    redirect_to movies_path
-  end
-
-  def edit
-    @movie = Movie.find params[:id]
-  end
-
-  def update
-    @movie = Movie.find params[:id]
-    @movie.update_attributes!(movie_params)
-    flash[:notice] = "#{@movie.title} was successfully updated."
-    redirect_to movie_path(@movie)
-  end
-
-  def destroy
-    @movie = Movie.find(params[:id])
-    @movie.destroy
-    flash[:notice] = "Movie '#{@movie.title}' deleted."
-    redirect_to movies_path
-  end
-
-  private
-  # Making "internal" methods private is not required, but is a common practice.
-  # This helps make clear which methods respond to requests, and which ones do not.
-  def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
-  end
-end
+    def show
+      id = params[:id] # retrieve movie ID from URI route
+      @movie = Movie.find(id) # look up movie by unique ID
+      # will render app/views/movies/show.<extension> by default
+    end
+  
+    def index
+    # Part 2 and Part 3 Section 
+      @items_sort = params[:sort] or session[:sort]
+      @movies_ratings = Movie.ratings
+      
+      
+      @entire_ratings =  params[:ratings] or session[:ratings]
+      
+      if not @entire_ratings
+        session[:ratings] = {}
+        
+        for rating in @movies_ratings
+          session[:ratings][rating] = 1
+        end
+        
+        @entire_ratings = session[:ratings]
+      end
+      
+      @movies = Movie.order(@items_sort)
+      if @entire_ratings
+        @movies = Movie.where(:rating=> @entire_ratings.keys).order(@items_sort)
+      end 
+      
+      if not params[:sort]==session[:sort] 
+        params[:ratings] = session[:ratings] = @entire_ratings
+        params[:sort] = session[:sort] = @items_sort
+        
+        flash.keep
+        
+        redirect_to movies_path(:sort=>params[:sort],:ratings=>params[:ratings])
+      end 
+      
+      if not params[:ratings]==session[:ratings]
+        params[:ratings] = session[:ratings] = @entire_ratings
+        params[:sort] = session[:sort] = @items_sort
+        
+        flash.keep
+        
+        redirect_to movies_path(:sort=>params[:sort],:ratings=>params[:ratings])
+      end
+        
+    end
+  
+    def new
+      # default: render 'new' template
+    end
+  
+    def create
+      @movie = Movie.create!(movie_params)
+      flash[:notice] = "#{@movie.title} was successfully created."
+      redirect_to movies_path
+    end
+  
+    def edit
+      @movie = Movie.find params[:id]
+    end
+  
+    def update
+      @movie = Movie.find params[:id]
+      @movie.update_attributes!(movie_params)
+      flash[:notice] = "#{@movie.title} was successfully updated."
+      redirect_to movie_path(@movie)
+    end
+  
+    def destroy
+      @movie = Movie.find(params[:id])
+      @movie.destroy
+      flash[:notice] = "Movie '#{@movie.title}' deleted."
+      redirect_to movies_path
+    end
+  
+    private
+    # Making "internal" methods private is not required, but is a common practice.
+    # This helps make clear which methods respond to requests, and which ones do not.
+    def movie_params
+      params.require(:movie).permit(:title, :rating, :description, :release_date)
+    end
+  end 
---
+++ rottenpotatoes/db/migrate/20211015025353_add_director_section.rb
@@ -0,0 +1,5 @@
+class AddDirectorSection < ActiveRecord::Migration
+  def change
+    add_column :movies, :director, :string
+  end
+end
